42125d4b4c699540a67358ba042407aa9b9cbdc9
Lou Bichard <lou.bichard@mudano.com>
2017-06-27 20:34:15 +0100

    :sparkles: Added form submission function, bound to the form

--- main.controller.js
+++ main.controller.js
@@ -1,6 +1,15 @@
 demoApp.controller('mainController', function($scope, countryService) {
 
+
     countryService.get().then(function(data){
         $scope.countries = data.data;
     }); 
+
+    $scope.formSubmit = function(){
+
+        // Guard against browsers where native form validation fails
+        if(!$scope.exampleForm.$valid) return;
+
+        console.log('submitted!');
+    };
 });
