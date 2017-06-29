e91c3d27383c0928ceb5692e85e97b07d44f7e63
Lou Bichard <lou.bichard@mudano.com>
2017-06-29 22:16:56 +0100

    :sparkles: Ensure the post request stores the data, not hardcoded values

--- public/components/form/form.directive.js
+++ public/components/form/form.directive.js
@@ -15,45 +15,39 @@ demoApp.directive('daForm', function(countryService, $state, userService) {
                 if (!$scope.exampleForm.$valid) return;
 
                 userService.create($scope.user)
-                    .then(function(){
-
-                        // TODO: Get from API? 
-                        userService.set($scope.user);
-
-                        $state.go('success');
-                    });
+                    .then(() => userService.set($scope.user))
+                    .then(() => $state.go('success'))
             };
 
         },
         scope: {},
         template: `
           <form name="exampleForm" ng-submit="formSubmit()">
-            <h1>Some useful government service</h1>
+            <h1>Some Useful Government Service</h1>
 
             <label for="name">Name</label>
             <input ng-model="user.name" required name="name" type="text">
             <br>
 
-            <label for="age">Age</label>
-            <input ng-model="user.age" required name="age" type="text">
-            <br>
-            
             <label for="sex">Sex</label>
             <span>Male</span>
             <input ng-model="user.sex" required type="radio" name="sex" value="male" checked>
             <span>Female</span>
             <input ng-model="user.sex" required type="radio" name="sex" value="female" checked>
             <br>
+
+            <label for="age">Age</label>
+            <input ng-model="user.age" required name="age" type="number">
+            <br>
     
             <label for="country">Country</label>
             <select required ng-model="user.country" name="country" ng-options="country.name as country.name for country in countries">
             </select>
             <br>
-
             <button type="submit">Apply</button>
 
             <div ng-if="!exampleForm.$valid && exampleForm.$submitted">
-                Please fill in everything!
+                Please ensure all fields are populated.
             </div>
 
             <div ui-view></div>
--- server/userdata/post.js
+++ server/userdata/post.js
@@ -3,11 +3,10 @@ const database = require('../database.js');
 module.exports = (req, res) => {
     database.getCollection('userdata').then((db) =>
         db.insertOne({
-            name: "Lou",
-            sex: "Male",
-            age: 12,
-            country: "United Kingdom",
-            dateCreated: new Date()
+            name: req.body.name,
+            sex: req.body.sex,
+            age: req.body.age,
+            country: req.body.country
         })
         .then((data) => res.send({
             success: true,
