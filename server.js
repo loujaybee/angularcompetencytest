774bde1429fd35091a4a9f30880ab7d1c3f60b26
Lou Bichard <lou.bichard@mudano.com>
2017-06-29 22:00:56 +0100

    :sparkles: Added node to serve the files statically

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app = express();
 
 app.use(bodyParser.json())
 
-app.get('/userdata', require('./server/userdata/post.js'));
+app.use(express.static(__dirname));
+
+app.get('api/userdata', require('./server/userdata/post.js'));
+
+app.get('/*', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
 
 app.listen(process.env.PORT || 8080);
