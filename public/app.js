741fb48b93d09478788db500a785a8542138d75f
Lou Bichard <lou.bichard@mudano.com>
2017-06-29 21:37:40 +0100

    :sparkles: Added ui router for states

--- public/app.js
+++ public/app.js
@@ -1,1 +1,19 @@
-demoApp = angular.module("demoApp", []);
+demoApp = angular.module("demoApp", ['ui.router']);
+
+demoApp.config(function($stateProvider, $locationProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise("/");
+    $locationProvider.html5Mode(true);
+
+    //Inside of the app config
+    $stateProvider
+        .state('home', {
+            url: '/',
+            template: '<da-form></da-form>'
+        })
+        .state('success', {
+            url: '/success',
+            template: '<da-success></da-success>'
+        });
+
+});
